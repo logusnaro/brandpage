9fb49a89655e7f3cce56aab184d08371e455e4ab
LogUs CEO <ceo@logus.ai>
2026-09-06 07:26:08 +0900

    fix: restore Sanity document publishing

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,10 +2,6 @@ import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { apiVersion, dataset, projectId } from "./src/sanity/env";
-import {
-  PublishStatusAction,
-  UnpublishStatusAction,
-} from "./src/sanity/lib/statusActions";
 import { schemaTypes } from "./src/sanity/schemaTypes";
 import { structure } from "./src/sanity/structure";
 
@@ -23,20 +19,6 @@ export default defineConfig({
     actions: (prev, context) => {
       if (context.schemaType === "siteSettings") {
         return prev.filter((action) => action.action !== "delete");
-      }
-
-      if (
-        context.schemaType === "product" ||
-        context.schemaType === "socialLink"
-      ) {
-        return [
-          PublishStatusAction,
-          UnpublishStatusAction,
-          ...prev.filter(
-            (action) =>
-              action.action !== "publish" && action.action !== "unpublish",
-          ),
-        ];
       }
 
       return prev;
